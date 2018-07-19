77aedbc3a0a078e4f03efbcd3f62abbacbf0b035
Gian Carlos Dal Pont <35267440+giandalpont@users.noreply.github.com>
2018-07-19 12:57:03 -0300

    Update App.js

--- webpack/src/App.js
+++ webpack/src/App.js
@@ -1,7 +1,15 @@
 'use strict'
 
-function sum   (val1, val2 ){
-    return val1 + val2
-} 
+// function sum   (val1, val2 ){
+//     return val1 + val2
+// } 
+// module.exports = sum
+// Criando aplicação em React
+var React = require('react') 
 
-module.exports = sum
+var Title = React.createClass({
+    render: function(){
+        return React.createElement('h1', null, 'Título')
+    }
+})
+module.exports = Title
