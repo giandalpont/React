745b6329bcc9653e5348caf07a3f6aacf2ab604b
Gian Carlos Dal Pont <35267440+giandalpont@users.noreply.github.com>
2018-07-20 13:45:31 -0300

    Update webpack.config.js

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,10 +3,24 @@
 const path = require('path')
 
 module.exports = {
+    devtool: 'source-map', //Debugar
+    
     entry: path.join(__dirname, 'src', 'index'),
+    
     output: {
         path: path.join(__dirname, 'dist'),
+        
         filename: 'bundle.js',
-        publicPath: '/dist/'
-    }
+        publicPath: '/static/'
+    },
+    
+    // Comfiguração do BABEL
+      module: {
+        rules: [{
+           test: /\.js$/,
+           exclude: /node_modules/,
+           include: /src/,
+           loader: 'babel-loader'
+    }]
+  }
 }
